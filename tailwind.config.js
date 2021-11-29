b86620b3af2bdad890d2a026ac589fb3b9237438
Raúl López <hi@raullg.com>
2021-11-29 19:21:49 +0000

    add netflix red

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
       './src/**/*.ts',
@@ -5,10 +7,28 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...colors,
+      red: {
+        "50": "#10010e10d",
+        "100": "#fef0f1",
+        "200": "#fcb1b5",
+        "300": "#f9767d",
+        "400": "#f63740",
+        "500": "#e60a15",
+        "600": "#ba0811",
+        "700": "#89060d",
+        "800": "#5d0409",
+        "900": "#310204"
+      }
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 }
